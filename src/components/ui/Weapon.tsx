00012c298d38cd67f066c68c0fc0ded22678a3c0
Tyler Slaughter <stslaug@gmail.com>
2026-03-22 21:05:16 -0400

    Added Basic Combat spells, and thieves ability

---
+++ src/components/ui/Weapon.tsx
@@ -0,0 +1,15 @@
+  // Component for a single weapon row in the table
+export function WeaponTableRow({ weapon }) {
+    return (
+      <tr>
+        <td className="p-2">{weapon.name}</td>
+        <td>{weapon.magAdj}</td>
+        <td>{weapon.range}</td>
+        <td>{weapon.speed}</td>
+    
+        {/* 2 columns for Damage VS Size S-M/L */}
+        <td>{weapon.damageSm}</td>
+        <td>{weapon.damageMl}</td>
+      </tr>
+    );
+  };
