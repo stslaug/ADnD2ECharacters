9b25ce2b0ac264a3af84dd5325ea5ce7431d29ae
Tyler Slaughter <stslaug@gmail.com>
2026-05-09 16:19:37 -0400

    I hate working with this github pages setup

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
-    output: isProd ? 'export' : undefined,
-    basePath: isProd ? '/ADnD2ECharacters' : undefined,
-    assetPrefix: isProd ? '/ADnD2ECharacters/' : undefined,
+    output: 'export',
+    basePath: '/ADnD2ECharacters',
+    assetPrefix: '/ADnD2ECharacters/',
     distDir: "build",
   images: {
     unoptimized: true,
